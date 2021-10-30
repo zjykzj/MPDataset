cf77514bdbed73c5f3ba27a03d2063c8a2a71b33
zjkjzj <wy163zhuj@163.com>
2021-10-30 18:28:50 +0800

    fix(get_cifar_data.py): specify fmt when save csv file

--- tools/data/get_cifar_data.py
+++ tools/data/get_cifar_data.py
@@ -53,7 +53,7 @@ def save_to_csv(data_list, csv_path):
     assert isinstance(data_list, list)
     assert not os.path.exists(csv_path)
 
-    np.savetxt(csv_path, np.array(data_list), delimiter=KEY_SEP)
+    np.savetxt(csv_path, np.array(data_list), delimiter=KEY_SEP, fmt='%s')
 
 
 if __name__ == '__main__':
@@ -61,23 +61,23 @@ if __name__ == '__main__':
     cfg_file = 'tools/data/cifar100.yaml'
     test_dataset = get_dataset(cfg_file, is_train=False)
 
-    dst_root = '/home/zj/data/cifar/imgs/test'
+    dst_root = 'data/cifar/imgs/test'
     data_list, class_list = process(test_dataset, dst_root)
 
-    csv_path = os.path.join('/home/zj/data/cifar/test/', KEY_DATASET)
+    csv_path = os.path.join('data/cifar/test/', KEY_DATASET)
     save_to_csv(data_list, csv_path)
 
-    csv_path = os.path.join('/home/zj/data/cifar/test/', KEY_CLASSES)
+    csv_path = os.path.join('data/cifar/test/', KEY_CLASSES)
     save_to_csv(class_list, csv_path)
 
     print('train ...')
     train_dataset = get_dataset(cfg_file, is_train=True)
 
-    dst_root = '/home/zj/data/cifar/imgs/train'
+    dst_root = 'data/cifar/imgs/train'
     data_list, class_list = process(train_dataset, dst_root)
 
-    csv_path = os.path.join('/home/zj/data/cifar/train/', KEY_DATASET)
+    csv_path = os.path.join('data/cifar/train/', KEY_DATASET)
     save_to_csv(data_list, csv_path)
 
-    csv_path = os.path.join('/home/zj/data/cifar/train/', KEY_CLASSES)
+    csv_path = os.path.join('data/cifar/train/', KEY_CLASSES)
     save_to_csv(class_list, csv_path)
